80f40e47c181656e07cdf6f11439ce0fb3c945b2
Petrouchka-umi <67958506+Petrouchka-umi@users.noreply.github.com>
2020-09-26 21:42:37 +0900

    app-edit

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -2,7 +2,7 @@ class Mood < ApplicationRecord
   belongs_to :message, optional: true
 
   def self.chart_date
-    order(created_at: :asc).pluck('created_at', 'status').to_h
-    # group_by_day(:created_at, format: "%m月%d日", series: false ).sum(:status)
+    # order(created_at: :asc).pluck('created_at', 'status').to_h
+    group_by_day(:created_at, format: "%m月%d日", series: false ).sum(:status)
   end
 end
